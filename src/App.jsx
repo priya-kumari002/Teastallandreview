24e29937e41eeeba1ec7e2d117e6e35f599331df
priya-kumari002 <priyakum2120@gmail.com>
2025-04-06 15:51:19 +0530

    priya

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
 
+// import React from 'react';
+// import { HashRouter  as Router, Routes, Route, Link } from 'react-router-dom';
+
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Product from "./components/Products";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+// import ReviewComponent from "./components/review";
+
+// import './App.css';
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="app">
+//         <nav className="navbar">
+//           <Link to="/">Home</Link>
+//           <Link to="/about">About</Link>
+//           <Link to="/products">Items</Link>
+//           <Link to="/contact">Contact</Link>
+//         </nav>
+
+//         <div className="content">
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/products" element={<Product />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/review" element={<ReviewComponent />} />
+//           </Routes>
+//         </div>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
 import React from 'react';
-import { HashRouter  as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from "./components/Home";
 import About from "./components/About";
@@ -11,7 +51,6 @@ import ReviewComponent from "./components/review";
 
 import './App.css';
 
-
 function App() {
   return (
     <Router>
@@ -21,6 +60,7 @@ function App() {
           <Link to="/about">About</Link>
           <Link to="/products">Items</Link>
           <Link to="/contact">Contact</Link>
+          <Link to="/review">Reviews</Link>
         </nav>
 
         <div className="content">
@@ -32,10 +72,12 @@ function App() {
             <Route path="/review" element={<ReviewComponent />} />
           </Routes>
         </div>
+
         <Footer />
       </div>
     </Router>
   );
+  
 }
 
 export default App;
